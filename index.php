3cc0f605e137d0253c761f1b757275911c66fa4a
Jane246 <janejennie042@gmail.com>
2023-01-19 22:16:05 +0700

    midterm

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>test</title>
+</head>
+<body>
+    <form action="show.php">
+        <h1>j</h1><!--มีได้ไม่เกิน6-->
+        <p title="I'm a tooltip">Attributeชื่อเรื่อง</p><!--มีข้อมูลเมื่อเอาเมาส์ชี้-->
+        <p>a</p><!--ใส่เมื่อต้องการย่อหน้าใหม่-->
+        <a href="https://www.w3schools.com">ใส่link</a><br>
+        <img src="w3schools.jpg" alt="รูปภาพ" width="104" height="142"><br>
+        <p style="color:purple;">เปลี่ยนสีตัวหนังสือ</p>
+        <body style="background-color:powderblue;">
+        <p style="background-color:pink;">พื้นหลังแค่บรรทัดข้อความนี้</p>
+        <!--เปลี่ยนฟอนต์ ขนาด ตำแหน่ง font-family,font-size,text-align-->
+        <!--การบ้าน-->
+        What is your name:<input type = "text" name = "name"><br>
+        <br><!--อยากเว้นบรรทัด-->
+        What is your age:<input type = "text" name = "age"><br>
+        What is your gender: <br>
+        <input type="radio" name="gender" value="male"> male<br>
+        <input type="radio" name="gender" value="female"> female<br>
+        What is your marrital status:<br>
+        <input type = "radio" name = "marrital_status" value="single"> single<br>
+        <input type = "radio" name = "marrital_status" value="married"> married<br>
+        <input type = "radio" name = "marrital_status" value="divorced"> divorced<br>
+        <input type = "radio" name = "marrital_status" value="widowed"> widowed<br>
+        <input type = "submit" value = "sumit">        
+    </form>  
+</body>
+</html>
---
+++ show.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+        $name = $_GET["name"];//ประกาศตัวแปรที่รับมาจากindex
+        $age = $_GET["age"];
+        $gender = $_GET["gender"];
+        $marrital_status = $_GET["marrital_status"];
+        $name_title = "";
+        $male = "male";//ประกาศตัวแปร
+        $female = "female";
+        $married = "married";
+        $widowed = "widowed";
+        if ($gender == $male)
+        {if ($age <= 14)
+            {$name_title = "ด.ช";}
+            else
+            {$name_title = "นาย";}}
+        else if ($gender == $female)
+        {if ($age <= 14)
+            {$name_title = "ด.ญ";}
+            else if ($marrital_status==$married||$marrital_status==$widowed)
+            {$name_title = "นาง";}
+            else{$name_title = "นางสาว";}}
+        echo "Name : " .$name_title. " ".$name."<br>";
+        echo "Age : " .$age."<br>";
+        echo "Gender : " .$gender."<br>";
+        echo "Marrital status : " .$marrital_status."<br>";
+    ?>
+</body>
+</html>
